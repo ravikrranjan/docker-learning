3c3d2055a1905a4f45633c9af63ca05340d724ef
ravikrranjan <raviranjan1600@gmail.com>
2020-04-11 12:40:07 +0530

    added express server code

--- docker-node-cloud-angular/server/server.js
+++ docker-node-cloud-angular/server/server.js
@@ -1,17 +1,35 @@
-const http = require('http');
-const hostname = 'localhost';
-const port = 3000;
+// const http = require('http');
+// const HOST = '0.0.0.0';
+// const port = 3000;
 
-const createServer = (req, res) => {
+// const createServer = (req, res) => {
 
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify({
-    'products': ['Rice', 'Wheat', 'Lemon']
-  }));
-};
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'application/json');
+//   res.end(JSON.stringify({
+//     'products': ['Rice', 'Wheat', 'Lemon']
+//   }));
+// };
 
-const server = http.createServer(createServer);
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}`)
-})
+// const server = http.createServer(createServer);
+// server.listen(port, HOST, () => {
+//   console.log(`Server running at http://${HOST}:${port}`)
+// })
+
+
+'use strict';
+
+const express = require('express');
+
+// Constants
+const PORT = 3000;
+const HOST = '0.0.0.0';
+
+// App
+const app = express();
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
